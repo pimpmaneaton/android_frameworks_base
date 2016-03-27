e44bd15c422e801c39fe11400c902ac4a26b4ab0
Lars Greiss <kufikugel@googlemail.com>
2016-03-26 21:18:14 -0400

    [1/2] base: HeadsUp add timeout option
    Forward ported to marshmallow/nougat By: @BeansTown106
    this was basically a rewrite as everything about it changed
    
    Change-Id: Ia8afb6494a3feb1f4f2ff6c13e8b6444c38a7506

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4841,22 +4841,6 @@ public final class Settings {
         public static final String PA_PIE_ENABLE_COLOR = "pa_pie_enable_color";
 
         /**
-<<<<<<< HEAD
-=======
-         * Defines the global heads up notification snooze
-         * @hide
-         */
-        public static final String HEADS_UP_NOTIFICATION_SNOOZE = "heads_up_notification_snooze";
-
-        /**
-         * Settings to backup. This is here so that it's in the same place as the settings
-         * keys and easy to update.
-         *
-         * NOTE: Settings are backed up and restored in the order they appear
-         *       in this array. If you have one setting depending on another,
-         *       make sure that they are ordered appropriately.
-         *
->>>>>>> 11ef994... [1/2] base: HeadsUp snooze function
          * @hide
          */
         public static final String PA_PIE_JUICE = "pa_pie_juice";
@@ -5900,6 +5884,12 @@ public final class Settings {
         public static final String HEADS_UP_NOTIFICATION_SNOOZE = "heads_up_notification_snooze";
 
         /**
+         * Heads up timeout configuration
+         * @hide
+         */
+        public static final String HEADS_UP_TIMEOUT = "heads_up_timeout";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
@@ -6086,6 +6076,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(QS_COLUMNS_PORTRAIT);
             PRIVATE_SETTINGS.add(QS_COLUMNS_LANDSCAPE);
 			PRIVATE_SETTINGS.add(HEADS_UP_NOTIFICATION_SNOOZE);
+			PRIVATE_SETTINGS.add(HEADS_UP_TIMEOUT);
         }
 
         /**
