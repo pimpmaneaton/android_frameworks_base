979a105dd9ff96ca097388e6c308d6ccc3523eff
ezio84 <brabus84@gmail.com>
2018-06-23 11:01:53 +0200

    Qs footer calendar/alarm: fix onClick ripple and cleanup code
    Change-Id: I72922db89bbe431481fdafc574e12920bcca8e4e

--- packages/SystemUI/src/com/android/systemui/qs/QSFooterImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSFooterImpl.java
@@ -132,7 +132,6 @@ public class QSFooterImpl extends FrameLayout implements Tunable, QSFooter,
 
         mDateTimeGroup = findViewById(id.date_time_alarm_group);
         mDate = findViewById(R.id.date);
-        mDate.setOnClickListener(this);
 
         mExpandIndicator = findViewById(R.id.expand_indicator);
         mSettingsButton = findViewById(R.id.settings_button);
@@ -144,7 +143,7 @@ public class QSFooterImpl extends FrameLayout implements Tunable, QSFooter,
 
         mAlarmStatusCollapsed = findViewById(R.id.alarm_status_collapsed);
         mAlarmStatus = findViewById(R.id.alarm_status);
-        mAlarmStatus.setOnClickListener(this);
+        mDateTimeGroup.setOnClickListener(this);
 
         mMultiUserSwitch = findViewById(R.id.multi_user_switch);
         mMultiUserAvatar = mMultiUserSwitch.findViewById(R.id.multi_user_avatar);
@@ -428,22 +427,20 @@ public class QSFooterImpl extends FrameLayout implements Tunable, QSFooter,
             } else {
                 startSettingsActivity();
             }
-        } else if (v == mAlarmStatus) {
+        } else if (v == mDateTimeGroup) {
+            if (mAlarmShowing) {
             Dependency.get(MetricsLogger.class).action(ACTION_QS_DATE,
                     mNextAlarm != null);
             if (mNextAlarm != null) {
                 PendingIntent showIntent = mNextAlarm.getShowIntent();
                 mActivityStarter.startPendingIntentDismissingKeyguard(showIntent);
-            } else {
-                mActivityStarter.postStartActivityDismissingKeyguard(new Intent(
-                        AlarmClock.ACTION_SHOW_ALARMS), 0);
+           } else {
+                Uri.Builder builder = CalendarContract.CONTENT_URI.buildUpon();
+                builder.appendPath("time");
+                builder.appendPath(Long.toString(System.currentTimeMillis()));
+                Intent todayIntent = new Intent(Intent.ACTION_VIEW, builder.build());
+                mActivityStarter.postStartActivityDismissingKeyguard(todayIntent, 0);
             }
-        } else if (v == mDate) {
-            Uri.Builder builder = CalendarContract.CONTENT_URI.buildUpon();
-            builder.appendPath("time");
-            builder.appendPath(Long.toString(System.currentTimeMillis()));
-            Intent todayIntent = new Intent(Intent.ACTION_VIEW, builder.build());
-            mActivityStarter.postStartActivityDismissingKeyguard(todayIntent, 0);
         } else if (v == mRunningServicesButton) {
             MetricsLogger.action(mContext,
                     mExpanded ? MetricsProto.MetricsEvent.ACTION_QS_EXPANDED_SETTINGS_LAUNCH
