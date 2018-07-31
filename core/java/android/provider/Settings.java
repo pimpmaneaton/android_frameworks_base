2be6813d034f4174e53a2d80b95d3c406dcd97e5
PMS22 <prathams99@rediff.com>
2018-07-30 19:17:25 -0600

    Add Flashlight blink on incoming calls [1/3]
    Change-Id: Ie7a77a7cb145e724c0bf9828a029e7993101a400

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5462,6 +5462,16 @@ public final class Settings {
          */
         public static final String POWERMENU_LS_FLASHLIGHT = "powermenu_ls_flashlight";
 
+           /**
+         * Whether to blink flashlight for incoming calls
+         * 0 = Disabled (Default)
+         * 1 = Blink flashlight only in Ringer mode
+         * 2 = Blink flashlight only in DND mode
+         * 3 = Blink flashlight always regardless of ringer mode
+         * @hide
+         */
+        public static final String FLASHLIGHT_ON_CALL = "flashlight_on_call";
+
         /**
          * Whether to use gesture anywhere feature.
          * @hide
